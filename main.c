7ac9a8a60623b3978245ffd353e0b69cc10a9fc1
danmo87 <133652735+danmo87@users.noreply.github.com>
2024-05-15 14:06:25 +0200

    commit because I'm soooo cool

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <nrfx.h>
+#include <nrf5340_application.h>
+#include <nrfx_config.h>
+#include <nrf.h>
+#include <nrfx_bsdriver.h>
+#include <assert.h>
+#include <stdio.h>
+
+/* By Daniel Morberg*/
+
+
+
+int main(void)
+{
+
+	int looper = 0;
+	int looper2 = 1;
+	int looper3 = 2;
+	uint8_t *secret_ptr = &secret;
+	uint8_t *keyword_ptr = &keyword;
+	uint8_t *goal_ptr = &goal;
+
+	while (looper < goal && looper2 < goal && looper3 < goal)
+	{
+		looper = looper + inkr1;
+		looper2 = looper2 + inkr2;
+		looper3 = looper3 + inkr3;
+	}
+}
